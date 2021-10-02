8b32918a19dec55a772155c6ed20eabb726a4b15
Amit Kumar <amit.kumar5@1mg.com>
2021-10-02 17:31:38 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const compression = require("compression");
+const helmet = require("helmet");
+const path = require("path");
+const fs = require("fs");
+const chalk = require("chalk");
+const cron = require("node-cron");
+
+const port = process.env.PORT || 3001;
+app.use(compression());
+app.use(helmet());
+
+app.listen(port, function () {
+  console.log("Coronavirus Tracker service on port 💣", port);
+  cron.schedule("*/10 * * * *", () => {
+    getCovidData();
+  });
+});
+
+app.get("/covid_data", function (req, res) {
+  const data = readFromFileSystem("/covid_data.txt", res);
+  res.status(200).send(data);
+});
+
+function getCovidData() {
+  axios
+    .get("https://www.mohfw.gov.in/data/datanew.json")
+    .then(function (res) {
+      saveTofileSystem(res.data, "covid_data.txt");
+    })
+    .catch(function (err) {
+      console.log(
+        "ERROR: while fetching data from source, retrying in 10mins\n",
+        err
+      );
+    });
+}
+
+function saveTofileSystem(obj, fileName) {
+  fs.writeFileSync(
+    path.join(__dirname + `/${fileName}`),
+    JSON.stringify(obj),
+    function (err) {
+      if (err) {
+        return console.log("ERROR: while writing to file\n", err);
+      }
+    }
+  );
+}
+
+function readFromFileSystem(fileName) {
+  return fs.readFileSync(
+    path.join(__dirname + `/${fileName}`),
+    "utf8",
+    function (err, data) {
+      if (err) {
+        console.log("ERROR: while reading file " + fileName + "\n", err);
+        return;
+      }
+    }
+  );
+}
